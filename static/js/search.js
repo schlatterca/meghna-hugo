c03b2be6d5bda40e1978d45b7aac4ba2d8a61350
schlatterca <48732275+schlatterca@users.noreply.github.com>
2021-07-20 19:17:30 +0200

    Update search.js

--- static/js/search.js
+++ static/js/search.js
@@ -70,10 +70,12 @@ function populateResults(result){
     var output = render(templateDefinition,{key:key,title:value.item.title,link:value.item.permalink,tags:value.item.tags,categories:value.item.categories,snippet:snippet});
     $('#search-results').append(output);
 
+    /*momentarily comment the highlight part…
     $.each(snippetHighlights,function(snipkey,snipvalue){
       $("#summary-"+key).mark(snipvalue);
     });
-
+    */
+    
   });
 }
 
