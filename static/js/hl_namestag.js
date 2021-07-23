9fb434303985420df44a6953a4731b55f28e668e
schlatterca <48732275+schlatterca@users.noreply.github.com>
2021-07-23 17:17:31 +0200

    Update hl_namestag.js

--- static/js/hl_namestag.js
+++ static/js/hl_namestag.js
@@ -53,8 +53,10 @@ function persontags(data){
 
           if (data.length == 3) { //rearrange
             var SurnameName = data[2] + ' ' + data[1] + ' ' + data[0];
+            return
           } else if (data.length == 2) {
             var SurnameName = data[1] + ' ' + data[0];
+            return
           }
 
           sortedNames.push(SurnameName); //and append it to the sortedNames array.
