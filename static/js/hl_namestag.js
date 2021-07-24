4f37a559f8475ddf8c34a40ff00970c775ba0a72
schlatterca <48732275+schlatterca@users.noreply.github.com>
2021-07-24 15:18:47 +0200

    Update hl_namestag.js

--- static/js/hl_namestag.js
+++ static/js/hl_namestag.js
@@ -71,14 +71,16 @@ function persontags(data){
       }
     }
   }
+
   //sortedNames.sort(); //sort sortedNames alphabetically.
-  sortedNames.sort(function(a, b) {
+  sortedNames.element.sort(function(a, b) {
     console.log(a.element.name, b.element.name);
     return parseFloat(a.element.name) - parseFloat(b.element.name);
   });
   //sortedNames.sort((a, b) => (a.element.color > b.element.color) ? 1 : -1)
   //sortedNames.sort((a, b) => parseFloat(a.element.name) - parseFloat(b.element.name));
   console.log(sortedNames);
+  console.log(sortedNames.element);
   //let uniqueSortedNames = [...new Set(sortedNames)]; //erase duplicates and get final array.
 
 
