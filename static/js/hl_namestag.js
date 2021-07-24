beef7d834d69f9968d1650009b10aa003a56031e
schlatterca <48732275+schlatterca@users.noreply.github.com>
2021-07-24 15:13:51 +0200

    Update hl_namestag.js

--- static/js/hl_namestag.js
+++ static/js/hl_namestag.js
@@ -76,6 +76,7 @@ function persontags(data){
     console.log(a.element.name, b.element.name);
     return parseFloat(a.element.name) - parseFloat(b.element.name);
   });
+  //sortedNames.sort((a, b) => (a.element.color > b.element.color) ? 1 : -1)
   //sortedNames.sort((a, b) => parseFloat(a.element.name) - parseFloat(b.element.name));
   console.log(sortedNames);
   //let uniqueSortedNames = [...new Set(sortedNames)]; //erase duplicates and get final array.
