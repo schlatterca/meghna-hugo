575bddbf4f38c86d18a4681020afb3fe40fbf3ea
schlatterca <48732275+schlatterca@users.noreply.github.com>
2021-07-26 21:45:47 +0200

    Update hl_namestag.js

--- static/js/hl_namestag.js
+++ static/js/hl_namestag.js
@@ -89,7 +89,7 @@ function persontags(data){
     if ((i != 0)&&(sortedNames[i].name == sortedNames[i-1].name)){
       console.log(sortedNames[i].name, sortedNames[i-1].name);
       //sortedNames[i-1].link = sortedNames[i].link;
-      sortedNames[i-1].link.push(sortedNames[i].link);
+      sortedNames[i-1].link.push(sortedNames[i].link[0]);
     }
   }
   
