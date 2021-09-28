ddb9e1cbab4ae008d35420798b5c2352611f9128
schlatterca <48732275+schlatterca@users.noreply.github.com>
2021-09-28 17:17:06 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -95,11 +95,10 @@ if (authors_without_by.includes(', ')){
 
     var name = document.createElement('a');
 		var nameText = document.createTextNode(single_authors[i]);
-
+		name.setAttribute('href', 'https://harfenlabor.netlify.app/persontags#'+author);
 		console.log(name, nameText);
-		/*name.setAttribute('href', 'https://harfenlabor.netlify.app/persontags#'+author);
 
-		if (i>0) {
+		/*if (i>0) {
 			name.append(', ');
 		}
 
