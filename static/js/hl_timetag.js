ca63c52e917f9b62b4bad086240206b2ea12d268
schlatterca <48732275+schlatterca@users.noreply.github.com>
2021-10-19 16:25:42 +0200

    Update hl_timetag.js

--- static/js/hl_timetag.js
+++ static/js/hl_timetag.js
@@ -1,6 +1,6 @@
-getJsonArray();
+getJsonArray_timeline();
 
-function getJsonArray(){
+function getJsonArray_timeline(){
   $.ajax({
       url: '/index.json',
       type: 'GET',
@@ -8,7 +8,7 @@ function getJsonArray(){
   })
 }
 
-function clean(array) {
+function clean_timeline(array) {
   for (var key in array) {
     if (array[key] === null || array[key] === undefined) {
       delete array[key];
@@ -17,16 +17,16 @@ function clean(array) {
   return array
 }
 
-var allNames = [];
-var allLinks= [];
-var allTitles= [];
+var allNames_timeline = [];
+var allLinks_timeline = [];
+var allTitles_timeline = [];
 
 function timelinetags(data){
   json = data; //fetch my json
   for (var key in json) { //for each key in the json…
     if (json.hasOwnProperty(key)) { //unless that key is not used…
       var dirtyArray = json[key]; //create an array of those results…
-      var result = clean(dirtyArray); //and clean it.
+      var result = clean_timeline(dirtyArray); //and clean it.
       if (result.hasOwnProperty("timelinetags")) { //and if the key "personags" exists…
         for (let i = 0; i < result.timelinetags.length; i++) { //for each result in "timelinetags"…
 
@@ -37,15 +37,15 @@ function timelinetags(data){
             SurnameName = SurnameName.replace('ç', 'c');
           }
 
-          allNames.push(SurnameName);
-          allLinks.push(result.permalink);
-          allTitles.push(result.title);
+          allNames_timeline.push(SurnameName);
+          allLinks_timeline.push(result.permalink);
+          allTitles_timeline.push(result.title);
         }
       }
     }
   }
 
-  const sortedNames = allNames.map((key, ind) => ({ 'name': key, 'link': [allLinks[ind]], 'title': [allTitles[ind]]}));
+  const sortedNames = allNames_timeline.map((key, ind) => ({ 'name': key, 'link': [allLinks_timeline[ind]], 'title': [allTitles_timeline[ind]]}));
   sortedNames.sort((a, b) => (a.name > b.name) ? 1 : -1);
 
   //adjust, remove duplicates
@@ -58,10 +58,131 @@ function timelinetags(data){
     }
   }
 
-  populateWithResults(sortedNames);
-  makeItInteractive();
+  populateWithResults_timeline(sortedNames);
+  makeItInteractive_timeline();
   return false;
 }
 
 
 
+function populateWithResults_timeline(myResults){
+  var templateDefinition = $('#timetag-result').html();
+
+  for (var i = 0; i < myResults.length; i++) {
+    //console.log(myResults[i].name, myResults[i].link, myResults[i].title);
+
+    const persona = document.createElement("span");
+    const quinome = document.createElement("span");
+    const spaceAfter = document.createElement("span");
+
+    //persona.id = "persona";
+
+    if (myResults[i].name.includes(" ")){
+      var nameForID = myResults[i].name.replaceAll(" ", "%20");
+      persona.id = nameForID;
+      persona.setAttribute("class", "persona");
+    } else {
+      persona.id = myResults[i].name;
+    }
+
+    persona.setAttribute("style", "margin-top: 10px; line-height: 0.6em; cursor: pointer;");
+    quinome.id = "quinome";
+    quinome.setAttribute("style", "font-size: 20px; color: black; margin-bottom: 0px; margin-top: 20px;");
+
+    quinome.innerHTML = myResults[i].name;
+    persona.append(quinome);
+    spaceAfter.innerHTML = "&nbsp;";
+    persona.append(spaceAfter);
+
+    for (var j = 0; j < myResults[i].link.length; j++) {
+      const quilink = document.createElement("a");
+      quilink.id = "quilink";
+      quilink.setAttribute("href", myResults[i].link[j]);
+      quilink.setAttribute("style", "font-size: 16px; color: grey;")
+      quilink.innerHTML = myResults[i].title[j]+"<br>";
+      //persona.append(quilink);
+    }    
+
+    $('#timelinetags-search-results').append(persona);
+
+    //make a box for each result
+    const indexBox = document.createElement("div");
+
+    if (myResults[i].name.includes(" ")){
+      var nameForID = myResults[i].name.replaceAll(" ", "-");
+      indexBox.id = nameForID;
+    } else {
+      indexBox.id = myResults[i].name;
+    }
+
+    indexBox.setAttribute('class', 'index_box');
+    const indexBoxInside = document.createElement("div");
+    indexBoxInside.setAttribute('class', 'index_box_inside');
+    indexBoxInside.style.width = "100%";
+    const indexBoxName = document.createElement("p");
+    indexBoxName.setAttribute('class', 'index_box_name');
+    const indexBoxText = document.createElement("p");
+    indexBoxText.setAttribute('class', 'index_box_text');
+    const closeIndexBox = document.createElement("div");
+    closeIndexBox.setAttribute('class', 'close_index_box');
+
+    indexBoxName.innerHTML = myResults[i].name;
+    closeIndexBox.innerHTML = "+";
+
+    for (var j = 0; j < myResults[i].link.length; j++) {
+      const quilink = document.createElement("a");
+      quilink.id = "quilink";
+      quilink.setAttribute("href", myResults[i].link[j]);
+      quilink.innerHTML = myResults[i].title[j]+"<br>";
+      indexBoxText.append(quilink);
+    }
+    indexBoxInside.append(indexBoxName);
+    indexBoxInside.append(indexBoxText);
+    indexBox.append(indexBoxInside);
+    indexBox.append(closeIndexBox);
+    $('#indexboxspace').append(indexBox);
+  }
+
+  //Delay and scroll down to selected ID
+  var url = window.location.href;
+  if(url.includes('#')) {
+    var url = url.split('#');
+    document.getElementById(url[1]).scrollIntoView({ behavior: 'smooth'});
+    document.getElementById(url[1]).children[0].style.color = "#B44BEB";
+  }
+};
+
+function makeItInteractive_timeline(){
+  $('span#quinome').each(
+    function() {
+      $(this).on("mouseover", function(e) {
+
+        $('span#quinome').each(
+          function() {
+            $(this).css('color', 'black');
+          }
+        );
+
+        $(this).css('color', '#B44BEB');
+      });
+      $(this).on("mouseout", function(e) {
+        $(this).css('color', 'black');
+      });
+
+      $(this).on("click", function(e) {
+        indexBackground.style.display = "block";
+        var thisIndexBox = this.textContent.replaceAll(" ", "-");
+        $("#"+thisIndexBox+".index_box").css('display', 'flex');
+      });
+    }
+  );
+
+  $(".close_index_box").on("click", function(e) {
+    document.getElementById("indexBackground").style.display="none";
+    $('.index_box').each(
+      function() {
+        $(this).css('display', 'none');
+      }
+    );
+  });
+}
