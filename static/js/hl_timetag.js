9b2f366a2746470f61d83abc83b582be6a75de16
schlatterca <48732275+schlatterca@users.noreply.github.com>
2021-10-19 17:38:28 +0200

    Update hl_timetag.js

--- static/js/hl_timetag.js
+++ static/js/hl_timetag.js
@@ -1,6 +1,6 @@
-getJsonArray_timeline();
+getJsonArray_tl();
 
-function getJsonArray_timeline(){
+function getJsonArray_tl(){
   $.ajax({
       url: '/index.json',
       type: 'GET',
@@ -8,7 +8,7 @@ function getJsonArray_timeline(){
   })
 }
 
-function clean_timeline(array) {
+function clean_tl(array) {
   for (var key in array) {
     if (array[key] === null || array[key] === undefined) {
       delete array[key];
@@ -17,17 +17,17 @@ function clean_timeline(array) {
   return array
 }
 
-var allNames_timeline = [];
-var allLinks_timeline = [];
-var allTitles_timeline = [];
+var allNames_tl = [];
+var allLinks_tl = [];
+var allTitles_tl = [];
 
 function timelinetags(data){
-  json = data; //fetch my json
-  for (var key in json) { //for each key in the json…
-    if (json.hasOwnProperty(key)) { //unless that key is not used…
-      var dirtyArray = json[key]; //create an array of those results…
-      var result = clean_timeline(dirtyArray); //and clean it.
-      if (result.hasOwnProperty("timelinetags")) { //and if the key "personags" exists…
+  json_tl = data; //fetch my json
+  for (var key in json_tl) { //for each key in the json…
+    if (json_tl.hasOwnProperty(key)) { //unless that key is not used…
+      var dirtyArray = json_tl[key]; //create an array of those results…
+      var result = clean_tl(dirtyArray); //and clean it.
+      if (result.hasOwnProperty("timelinetags")) { //and if the key "persona_tlgs" exists…
         for (let i = 0; i < result.timelinetags.length; i++) { //for each result in "timelinetags"…
 
           var SurnameName = result.timelinetags[i];
@@ -37,102 +37,102 @@ function timelinetags(data){
             SurnameName = SurnameName.replace('ç', 'c');
           }
 
-          allNames_timeline.push(SurnameName);
-          allLinks_timeline.push(result.permalink);
-          allTitles_timeline.push(result.title);
+          allNames_tl.push(SurnameName);
+          allLinks_tl.push(result.permalink);
+          allTitles_tl.push(result.title);
         }
       }
     }
   }
 
-  const sortedNames = allNames_timeline.map((key, ind) => ({ 'name': key, 'link': [allLinks_timeline[ind]], 'title': [allTitles_timeline[ind]]}));
-  sortedNames.sort((a, b) => (a.name > b.name) ? 1 : -1);
+  const sortedNames_tl = allNames_tl.map((key, ind) => ({ 'name': key, 'link': [allLinks_tl[ind]], 'title': [allTitles_tl[ind]]}));
+  sortedNames_tl.sort((a, b) => (a.name > b.name) ? 1 : -1);
 
   //adjust, remove duplicates
-  for (var i = 0; i < sortedNames.length; i++) {
-    if ((i != 0)&&(sortedNames[i].name == sortedNames[i-1].name)){
-      sortedNames[i-1].link.push(sortedNames[i].link[0]);
-      sortedNames[i-1].title.push(sortedNames[i].title[0]);
-      sortedNames.splice(i, 1);
+  for (var i = 0; i < sortedNames_tl.length; i++) {
+    if ((i != 0)&&(sortedNames_tl[i].name == sortedNames_tl[i-1].name)){
+      sortedNames_tl[i-1].link.push(sortedNames_tl[i].link[0]);
+      sortedNames_tl[i-1].title.push(sortedNames_tl[i].title[0]);
+      sortedNames_tl.splice(i, 1);
       i = i-1;
     }
   }
 
-  populateWithResults_timeline(sortedNames);
-  makeItInteractive_timeline();
+  populateWithResults_tl(sortedNames_tl);
+  makeItInteractive_tl();
   return false;
 }
 
 
 
-function populateWithResults_timeline(myResults){
+function populateWithResults_tl(myResults){
   var templateDefinition = $('#timetag-result').html();
 
   console.log(myResults);
 
   for (var i = 0; i < myResults.length; i++) {
 
-    const persona = document.createElement("span");
-    const quinome = document.createElement("span");
-    const spaceAfter = document.createElement("span");
+    const persona_tl = document.createElement("span");
+    const quinome_tl = document.createElement("span");
+    const spaceAfter_tl = document.createElement("span");
 
-    persona.id = myResults[i].name;
+    persona_tl.id = myResults[i].name;
 
-    persona.setAttribute("style", "margin-top: 10px; line-height: 0.6em; cursor: pointer;");
-    quinome.id = "quinome";
-    quinome.setAttribute("style", "font-size: 20px; color: black; margin-bottom: 0px; margin-top: 20px;");
+    persona_tl.setAttribute("style", "margin-top: 10px; line-height: 0.6em; cursor: pointer;");
+    quinome_tl.id = "quinome_tl";
+    quinome_tl.setAttribute("style", "font-size: 20px; color: black; margin-bottom: 0px; margin-top: 20px;");
 
-    quinome.innerHTML = myResults[i].name+" "; //DELETE
-    persona.append(quinome);
-    spaceAfter.innerHTML = "&nbsp;";
-    persona.append(spaceAfter);
+    quinome_tl.innerHTML = myResults[i].name+" "; //DELETE
+    persona_tl.append(quinome_tl);
+    spaceAfter_tl.innerHTML = "&nbsp;";
+    persona_tl.append(spaceAfter_tl);
 
     for (var j = 0; j < myResults[i].link.length; j++) {
-      const quilink = document.createElement("a");
-      quilink.id = "quilink";
-      quilink.setAttribute("href", myResults[i].link[j]);
-      quilink.setAttribute("style", "font-size: 16px; color: grey;")
-      quilink.innerHTML = myResults[i].title[j]+"<br>";
+      const quilink_tl = document.createElement("a");
+      quilink_tl.id = "quilink";
+      quilink_tl.setAttribute("href", myResults[i].link[j]);
+      quilink_tl.setAttribute("style", "font-size: 16px; color: grey;")
+      quilink_tl.innerHTML = myResults[i].title[j]+"<br>";
     }    
 
-    $('#timelinetags-search-results').append(persona);
+    $('#timelinetags-search-results').append(persona_tl);
 
     //make a box for each result
-    const indexBox = document.createElement("div");
+    const indexBox_tl = document.createElement("div");
 
     if (myResults[i].name.includes(" ")){
       var nameForID = myResults[i].name.replaceAll(" ", "-");
-      indexBox.id = nameForID;
+      indexBox_tl.id = nameForID;
     } else {
-      indexBox.id = myResults[i].name;
+      indexBox_tl.id = myResults[i].name;
     }
 
-    indexBox.setAttribute('class', 'index_box');
-    const indexBoxInside = document.createElement("div");
-    indexBoxInside.setAttribute('class', 'index_box_inside');
-    indexBoxInside.style.width = "100%";
-    const indexBoxName = document.createElement("p");
-    indexBoxName.setAttribute('class', 'index_box_name');
-    const indexBoxText = document.createElement("p");
-    indexBoxText.setAttribute('class', 'index_box_text');
-    const closeIndexBox = document.createElement("div");
-    closeIndexBox.setAttribute('class', 'close_index_box');
+    indexBox_tl.setAttribute('class', 'index_box');
+    const indexBox_tlInside = document.createElement("div");
+    indexBox_tlInside.setAttribute('class', 'index_box_inside');
+    indexBox_tlInside.style.width = "100%";
+    const indexBox_tlName = document.createElement("p");
+    indexBox_tlName.setAttribute('class', 'index_box_name');
+    const indexBox_tlText = document.createElement("p");
+    indexBox_tlText.setAttribute('class', 'index_box_text');
+    const closeindexBox_tl = document.createElement("div");
+    closeindexBox_tl.setAttribute('class', 'close_index_box');
 
-    indexBoxName.innerHTML = myResults[i].name;
-    closeIndexBox.innerHTML = "+";
+    indexBox_tlName.innerHTML = myResults[i].name;
+    closeindexBox_tl.innerHTML = "+";
 
     for (var j = 0; j < myResults[i].link.length; j++) {
-      const quilink = document.createElement("a");
-      quilink.id = "quilink";
-      quilink.setAttribute("href", myResults[i].link[j]);
-      quilink.innerHTML = myResults[i].title[j]+"<br>";
-      indexBoxText.append(quilink);
+      const quilink_tl = document.createElement("a");
+      quilink_tl.id = "quilink";
+      quilink_tl.setAttribute("href", myResults[i].link[j]);
+      quilink_tl.innerHTML = myResults[i].title[j]+"<br>";
+      indexBox_tlText.append(quilink_tl);
     }
-    indexBoxInside.append(indexBoxName);
-    indexBoxInside.append(indexBoxText);
-    indexBox.append(indexBoxInside);
-    indexBox.append(closeIndexBox);
-    $('#indexboxspace').append(indexBox);
+    indexBox_tlInside.append(indexBox_tlName);
+    indexBox_tlInside.append(indexBox_tlText);
+    indexBox_tl.append(indexBox_tlInside);
+    indexBox_tl.append(closeindexBox_tl);
+    $('#indexBox_tlspace').append(indexBox_tl);
   }
 
   //Delay and scroll down to selected ID
@@ -144,12 +144,12 @@ function populateWithResults_timeline(myResults){
   }
 };
 
-function makeItInteractive_timeline(){
-  $('span#quinome').each(
+function makeItInteractive_tl(){
+  $('span#quinome_tl').each(
     function() {
       $(this).on("mouseover", function(e) {
 
-        $('span#quinome').each(
+        $('span#quinome_tl').each(
           function() {
             $(this).css('color', 'black');
           }
@@ -163,8 +163,8 @@ function makeItInteractive_timeline(){
 
       $(this).on("click", function(e) {
         indexBackground.style.display = "block";
-        var thisIndexBox = this.textContent.substring(0, this.textContent.length - 1);
-        $("#"+thisIndexBox+".index_box").css('display', 'flex');
+        var thisindexBox_tl = this.textContent.substring(0, this.textContent.length - 1);
+        $("#"+thisindexBox_tl+".index_box").css('display', 'flex');
       });
     }
   );
